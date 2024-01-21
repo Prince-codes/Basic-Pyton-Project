f17135ddf26f8f82f343af715fabf56189e86f1b
Hackprince99 <silence11444@gmail.com>
2024-01-21 15:40:46 +0530

    Create auto_messanging.py

---
+++ auto_messanging.py
@@ -0,0 +1,19 @@
+import pyautogui as auto
+import time as t
+
+
+t.sleep(5)  
+
+for i in range (1121):
+    auto.hotkey('ctrl','v')
+    auto.press('enter')  
+
+              
+                
+                  
+                    
+                      
+                        
+                          
+                            
+                              
