6b9dff310c81c29cf52708ea54703be7b0c91e3b
Prince Raj Singh <silence11444@gmail.com>
2025-04-20 19:30:17 +0530

    Create qr_generated.py

---
+++ qr_generated.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# Taking URL input
+url = input("Enter the URL to generate a QR code: ")
+
+# Generate the QR code
+qr = qrcode.QRCode(
+    version=1,  # Controls the size of the QR Code
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  # Error correction level
+    box_size=10,  # Size of each box in the QR code grid
+    border=4,  # Thickness of the border (minimum is 4)
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image of the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the QR code image
+img.save("qrcode.png")
+
+print("QR code generated and saved as 'qrcode.png'.")
